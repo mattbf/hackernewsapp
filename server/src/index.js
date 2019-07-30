5b71e58e5c51c92675f955ca53e73bd402c0a305
mattbf <matthewbf8@gmail.com>
2019-07-30 15:55:30 -0600

    Created vote model, schema, and subscriptions

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
 const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
 
 const resolvers = {
@@ -12,7 +13,8 @@ const resolvers = {
   Mutation,
   Subscription,
   User,
-  Link
+  Link,
+  Vote
 }
 
 const server = new GraphQLServer({
--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -40,6 +40,26 @@ async function login(parent, args, context, info) {
   }
 }
 
+async function vote(parent, args, context, info) {
+  // 1
+  const userId = getUserId(context)
+
+  // 2
+  const linkExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link: { id: args.linkId },
+  })
+  if (linkExists) {
+    throw new Error(`Already voted for link: ${args.linkId}`)
+  }
+
+  // 3
+  return context.prisma.createVote({
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } },
+  })
+}
+
 function post(parent, args, context, info) {
   const userId = getUserId(context)
   return context.prisma.createLink({
@@ -53,4 +73,5 @@ module.exports = {
   signup,
   login,
   post,
+  vote
 }
