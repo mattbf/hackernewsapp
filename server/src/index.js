76bf094554f94189d84b85c3fe8b2822898293b0
mattbf <matthewbf8@gmail.com>
2019-07-30 11:46:34 -0600

    Added a feed query and Link type

--- server/src/index.js
+++ server/src/index.js
@@ -4,13 +4,31 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
+// Dummy data
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => null,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
