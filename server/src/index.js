6e3b5ac204cac516f582080c42de5e41aa7b93ca
mattbf <matthewbf8@gmail.com>
2019-07-30 15:12:17 -0600

    Refactored schema into separate files for modularity

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-// Schema
-const typeDefs = './src/schema.graphql'
 
 const resolvers = {
   Query: {
@@ -22,7 +21,8 @@ const resolvers = {
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
