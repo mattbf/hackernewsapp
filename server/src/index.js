3b3298038cdf77bbde0ef0b78708f8020642478b
mattbf <matthewbf8@gmail.com>
2019-07-30 14:48:35 -0600

    removed dummy data and updated resolvers

--- server/src/index.js
+++ server/src/index.js
@@ -3,47 +3,20 @@ const { GraphQLServer } = require('graphql-yoga')
 // Schema
 const typeDefs = './src/schema.graphql'
 
-// Dummy data
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-let idCount = links.length
-
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: function(id) {
-      if (!links[id]) {
-        throw new Error('no link with id:' + id)
-      }
-      return "links[id]"
-    }
+    feed: (root, args, context, info) => {
+      return context.prisma.links()
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      }
-      links.push(link)
-      return link
-    },
-    updateLink: (parent, args) => {
-      links[args.id] = {
-        id: args.id,
         description: args.description,
-        url: args.url
-      }
-      return links[args.id]
+      })
     },
-    deleteLink: (parent, args) => {
-      links.splice(args.id, 1)
-      return links[args.id]
-    }
   },
 }
 
