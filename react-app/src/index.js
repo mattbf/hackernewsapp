ff640a7c263e6d16054f901961e93fd08c980099
mattbf <matthewbf8@gmail.com>
2019-07-31 13:29:23 -0600

    Added token to headers of requests

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './Styles/index.css'
 import App from './Components/App'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
 // 1
 import { ApolloProvider } from 'react-apollo'
@@ -9,18 +11,25 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
-// 3
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
-// 4
 ReactDOM.render(
 
     <ApolloProvider client={client}>
