f05cef83768a8665c108a7ac3a708f43e6d3f162
mattbf <matthewbf8@gmail.com>
2019-07-31 10:52:34 -0600

    Auth is not setup so adding links causes error

--- react-app/src/Components/CreateLink.js
+++ react-app/src/Components/CreateLink.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -37,7 +39,9 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <button onClick={`... you'll implement this 🔜`}>Submit</button>
+        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+          {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
       </div>
     )
   }
